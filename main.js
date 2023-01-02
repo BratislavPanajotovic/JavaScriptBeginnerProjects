dd007a2b25cda02f5c74bd79fa48901290098614
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-02 15:36:41 +0100

    Switch

--- main.js
+++ main.js
@@ -43,44 +43,86 @@
 //console.log(markHigherBMI);
 
 
-/////////////// CODING CHALLENGE 3 
+/////////////// CODING CHALLENGE 3
 
-let scoreDolphins1 = 96;
-let scoreDolphins2 = 108;
-let scoreDolphins3 = 89;
+//let scoreDolphins1 = 96;
+//let scoreDolphins2 = 108;
+//let scoreDolphins3 = 119;
 
-let averageScoreDolphins = (scoreDolphins1 + scoreDolphins2 + scoreDolphins3) / 3;
-console.log(averageScoreDolphins);
+//let averageScoreDolphins = (scoreDolphins1 + scoreDolphins2 + scoreDolphins3) / 3;
+//console.log(averageScoreDolphins);
 
-let scoreKoalas1 = 88;
-let scoreKoalas2 = 91;
-let scoreKoalas3 = 110;
+//let scoreKoalas1 = 118;
+//let scoreKoalas2 = 91;
+//let scoreKoalas3 = 110;
 
-let averageScoreKoalas = (scoreKoalas1 + scoreKoalas2 + scoreKoalas3) / 3;
-console.log(averageScoreKoalas);
+//let averageScoreKoalas = (scoreKoalas1 + scoreKoalas2 + scoreKoalas3) / 3;
+//console.log(averageScoreKoalas);
 
-let minimumScore = 100;
-if ((averageScoreDolphins, averageScoreKoalas) < minimumScore) {
-    console.log('Both teams are below the minimum score and its a draw!');
-} else {
+//let minimumScore = 100;
+//if ((averageScoreDolphins, averageScoreKoalas) < minimumScore) {
+//    console.log('Both teams are below the minimum score and its a draw!');
+//} else {
 
-    if (averageScoreDolphins < averageScoreKoalas) {
+//    if (averageScoreDolphins < averageScoreKoalas) {
 
-        console.log('Koalas won !');
-    }
-
-
-    if (averageScoreDolphins > averageScoreKoalas) {
-
-        console.log('Dolphins won !');
-    }
-
-    if (averageScoreDolphins === averageScoreKoalas) {
-        console.log("Its a draw!");
+//        console.log('Koalas won !');
+//    }
 
 
-    }
+//    if (averageScoreDolphins > averageScoreKoalas) {
+
+//        console.log('Dolphins won !');
+//    }
+
+//   if (averageScoreDolphins === averageScoreKoalas) {
+//     console.log("Its a draw!");
+
+
+// }
+//}
+////////////////////////// SWITCH //////////////////////////////////////
+const day = 'wednesday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :D');
+        break;
+
+    default:
+        console.log('Not a valid day!');
 }
-//////////////////////////
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
 
+} else if (day === 'thuesday') {
+    console.log('Prepare theory videos');
 
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+
+} else if (day === 'friday') {
+    console.log('Record videos');
+
+} else if (day === 'satyurday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+
+} else console.log('Not a valid day');
+
+///////////////////////// SWITCH //////////////////////////////////////////
