1d304204766c0a103a021cd2ccd8d10e47c23ca5
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-03 21:56:37 +0100

    First commit

---
+++ 02-JS Fundamentals/README.md
@@ -0,0 +1,1 @@
+﻿# JS Fundamentals 1
---
+++ 02-JS Fundamentals/script.js
@@ -0,0 +1,28 @@
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive :D');
+
+//const interface = 'Audio';
+// const private = 534;
+
+function logger() {
+    console.log('Hi my name is, whaat my name is');
+}
+// calling / running / invoking function 
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
