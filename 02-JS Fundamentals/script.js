2ab032c9ea5277923c5c196828cad7ece9da49ed
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-05 18:31:15 +0100

    Function inside a function

--- 02-JS Fundamentals/script.js
+++ 02-JS Fundamentals/script.js
@@ -9,20 +9,49 @@
 //const interface = 'Audio';
 // const private = 534;
 
-function logger() {
-    console.log('Hi my name is, whaat my name is');
-}
+// function logger() {
+// console.log('Hi my name is, whaat my name is');
+// }
 // calling / running / invoking function 
 // logger();
 // logger();
 // logger();
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+////////////// FUNCTION EXPRESSION ///////////////////////
+
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+
+// //// ARROW FUNCTION //////////////////////
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+
+// const yearsUnitlRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return ` ${firstName} retires in ${retirement} years`;
+// }
+// console.log(yearsUnitlRetirement(1991, 'Jonas'));
+// console.log(yearsUnitlRetirement(1980, 'Boca'));
+const fruitCutter = function (fruit) {
+    return fruit * 4;
+}
+
+const juiceContainer = function (apples, oranges, peaches) {
+    const applePieces = fruitCutter(apples);
+    const orangePieces = fruitCutter(oranges);
+    const peachPieces = fruitCutter(peaches);
+    const juice = `This Juice contains ${applePieces} apple pieces, ${orangePieces} orange pieces and ${peachPieces} peach pieces !`;
     return juice;
 }
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+console.log(juiceContainer(5, 3, 4));
