0d6a0e17c536a17b72110b57bf7bcaa03865d10d
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-13 21:21:21 +0100

    Continue and break loop

--- 02-JS Fundamentals/script.js
+++ 02-JS Fundamentals/script.js
@@ -169,29 +169,53 @@
 // }
 
 
-const jonas = [
-    'Jonas',
-    'Schmedtmann',
-    1991,
-    'teacher',
-    ['Peter', 'Michael', 'Steven'],
-    true
-];
-const types = [];
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     1991,
+//     'teacher',
+//     ['Peter', 'Michael', 'Steven'],
+//     true
+// ];
+// const types = [];
+
+// for (let i = 0; i < jonas.length; i++) {
+//     console.log(jonas[i], typeof jonas[i]);
+
+//     // types[i] = typeof jonas[i];
+//     types.push(typeof jonas[i]);
+// };
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//     ages.push(2037 - years[i]);
+
+// }
+// console.log(ages);
+// //////////continue and break
+console.log('ONLY STRINGS');
 
 for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+
     console.log(jonas[i], typeof jonas[i]);
-
-    // types[i] = typeof jonas[i];
-    types.push(typeof jonas[i]);
 };
-console.log(types);
+console.log('BREAK WITH NUMBER');
 
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
 
-for (let i = 0; i < years.length; i++) {
-    ages.push(2037 - years[i]);
+    console.log(jonas[i], typeof jonas[i]);
+};
 
-}
-console.log(ages);
+// const jonas = [
+//     'Jonas',
+//     'Schmedtmann',
+//     1991,
+//     'teacher',
+//     ['Peter', 'Michael', 'Steven'],
+// ];
+
