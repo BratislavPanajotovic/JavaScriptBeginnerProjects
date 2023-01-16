14f0af4383a03169862df15c814a8ec9cefaee51
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-16 18:03:24 +0100

    Next Level

--- 02-JS Fundamentals/script.js
+++ 02-JS Fundamentals/script.js
@@ -246,33 +246,36 @@
 //     }
 // }
 /////////FINAL CODING CHALLENGE OF FUNDAMENTALS////////////////
+// const tips = [];
+// const totals = [];
+// let sum = 0;
 
-const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
 
-for (let i = 0; i < bills.length; i++) {
-    const tip = calcTip(bills[i]);
-    tips.push(tip);
-    totals.push(tip + bills[i]);
-}
-console.log(bills);
-console.log(tips);
-console.log(totals);
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// for (let i = 0; i < bills.length; i++) {
 
-const calcAverage = function (arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i];
-    }
-    console.log(sum);
-    return sum / arr.length;
-}
-console.log(calcAverage([2, 3, 6]));
-console.log(calcAverage([totals]));
+//     tips.push(calcTip(bills[i]));
+//     totals.push(calcTip(bills[i]) + bills[i]);
+// }
+
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
+
+
+
+// const calcAverage = function (arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+//     console.log(sum);
+//     return sum / arr.length;
+// }
+
+// console.log(calcAverage(totals));
 
 
 // const calcTip = function (bill) {
