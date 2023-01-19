0c33e5d5acdd11aa8e28584ca563ca469d5c4e13
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-19 16:54:24 +0100

    DebuggingChallenge

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,24 +1,24 @@
 // Remember, we're gonna use strict mode in all scripts now!
-// 'use strict';
-const t1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
-const t2 = [1, -2, -7, -1, 'error', 8, 13, 13, 15, 5, 9, 5];
-let tu = t1.concat(t2);
-const calcTempAmpl = function (temps) {
-  let maxTemp = temps[0];
-  let minTemp = temps[0];
+'use strict';
+// const t1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const t2 = [1, -2, -7, -1, 'error', 8, 13, 13, 15, 5, 9, 5];
+// let tu = t1.concat(t2);
+// const calcTempAmpl = function (temps) {
+//   let maxTemp = temps[0];
+//   let minTemp = temps[0];
 
-  for (i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > maxTemp) maxTemp = curTemp;
-    if (curTemp < minTemp) minTemp = curTemp;
-  }
-  console.log(maxTemp);
-  console.log(minTemp);
-  return Math.abs(maxTemp) - Math.abs(minTemp);
-};
-const amplitude = calcTempAmpl(tu);
-console.log(amplitude);
+//   for (i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > maxTemp) maxTemp = curTemp;
+//     if (curTemp < minTemp) minTemp = curTemp;
+//   }
+//   console.log(maxTemp);
+//   console.log(minTemp);
+//   return Math.abs(maxTemp) - Math.abs(minTemp);
+// };
+// const amplitude = calcTempAmpl(tu);
+// console.log(amplitude);
 
 // let maxTemp = temperatures[0];
 // let minTemp = temperatures[0];
@@ -38,3 +38,22 @@ console.log(amplitude);
 //   }
 //   return maxTemp - minTemp;
 // }
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celsius',
+//     value: Number(prompt('Degrees celsius:')),
+//   };
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// console.log(measureKelvin());
+const printForecast = function (values) {
+  let Text = [];
+  for (let i = 0; i < values.length; i++) {
+    Text.push(`${values[i]}℃ in ${i + 1} days...`);
+  }
+  return Text;
+};
+let temps = [12, 5, -5, 0, 4];
+console.log(printForecast(temps));
