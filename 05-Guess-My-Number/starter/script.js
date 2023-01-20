b86757de73d34ad73888cb5d5e891a1bed227b0b
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-21 00:31:25 +0100

    DOM of the game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,15 @@
 'use strict';
-document.querySelector('.message');
+// document.querySelector('.message').textContent = '🎉Correct Number!';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    const message = (document.querySelector('.message').textContent =
+      '⛔ No number!');
+    console.log(message);
+  }
+});
