5a7c2b7c3136d99f4a75193bf5fd8cc16c8676f9
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-21 01:59:52 +0100

    Logic interpreted

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -6,9 +6,11 @@
 const secretNumber = Math.round(Math.random() * 20);
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+
   if (!guess) {
     const message = (document.querySelector('.message').textContent =
       '⛔ No number!');
@@ -16,5 +18,26 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       '🎉 You have chosen wisely🦉!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        '👆 Your guess is higher than my number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😭 You lost the game!';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent =
+        '👇 Your guess is lower than my number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = '😭 You lost the game!';
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  onclick = refreshpage;
 });
