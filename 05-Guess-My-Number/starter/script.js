0c4c614afd733594bc158f1218c4ed389cd262fa
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-01-21 01:16:04 +0100

    AppProgress

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,6 +3,8 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value;
+const secretNumber = Math.round(Math.random() * 20);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -11,5 +13,8 @@ document.querySelector('.check').addEventListener('click', function () {
     const message = (document.querySelector('.message').textContent =
       '⛔ No number!');
     console.log(message);
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🎉 You have chosen wisely🦉!';
   }
 });
