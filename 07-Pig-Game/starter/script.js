f8a62890ae3ea51db987b578c9750686bba6242b
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-02-08 12:26:05 +0100

    game logic

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,14 +5,32 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
-diceEl.classList.add('hidden');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 
 score0El.textContent = 0;
-score0E2.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
 btnRoll.addEventListener('click', function () {
-  dice = Math.random(1, 6);
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
   diceEl.classList.remove('hidden');
-  if ((dice = 1)) {
+  console.log(dice);
+  diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    score0El.textContent = currentScore;
+  } else {
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
+console.log(currentScore);
+console.log(score0El.textContent);
