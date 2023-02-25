c9d2f1d4f8e32b4f4c1dedd2bbf6a5ab7ca8de43
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-02-25 22:55:15 +0100

    Rest vs Spread

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,12 +45,41 @@ const restaurant = {
       `Here is your delicious pasta with ${ing1},${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngridients) {
+    console.log(mainIngredient, otherIngridients);
+  },
 };
 
 const arr = [7, 8, 9, 10, 11, 12];
 const newArr = [1, 2, ...arr];
 const [a, b, ...others] = [arr];
 console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'spinach');
+
+////objects//
+
 // const newMenu = ['Gnochi', ...restaurant.mainMenu];
 // console.log(newMenu);
 
