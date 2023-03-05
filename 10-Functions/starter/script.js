2ed5db229c0464af2a46c3b3e9478d35a1f5f646
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-03-05 16:05:30 +0100

    Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -5,3 +5,30 @@ const newPassport = function (person) {
 };
 newPassport(jonas);
 console.log(jonas);
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string:${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('Javascript is the best', upperFirstWord);
+transformer('Javascript is the best', oneWord);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+let greet = greeting => name => console.log(`${greeting} ${name}`);
+
+greet('Hello')('Buddy');
