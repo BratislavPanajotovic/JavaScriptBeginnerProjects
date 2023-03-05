d1eb3389c2165b344fd4bd6d1ccfc50995780610
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-03-05 14:23:26 +0100

    Adv Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+const jonas = { name: 'Jonas Schmedtman', passport: 123123123 };
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 999999);
+};
+newPassport(jonas);
+console.log(jonas);
