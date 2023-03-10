9cda58e5fe1d332de4fbf230a94fda03dcd9c281
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-03-10 09:17:13 +0100

    Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,15 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+
+console.log(arr.splice(2));
+
+let arr2 = ['f', 'g', 'h', 'i', 'o'];
+const letters = console.log(arr.concat(arr2));
+console.log(letters.join(' - '));
