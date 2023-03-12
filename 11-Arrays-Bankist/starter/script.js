3bd0f2348e561ee79191e0a64a1d0d1f2a229e24
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-03-12 18:41:03 +0100

    reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -104,11 +104,13 @@ const depositsFor = [];
 for (const mov of movements) if (mov > 0) depositsFor.push(mov);
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
-const balance = movements.reduce(function (acc, cur, i, arr) {
-  console.log(`Iteration ${i}: ${acc}`);
-  return acc + cur;
-}, 0);
-console.log(balance);
+const balance = movements.reduce((acc, cur, i, arr) => acc + cur, 0,
+labelBalance.textContent = ``
+);
+
+
 let balance2 = 0;
 for (const mov of movements) balance2 += mov;
 console.log(balance2);
+
+const calcAndDisplayBalance = ;
