d62322208bec36749b4bed6fd614018088f77189
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2023-03-12 18:18:57 +0100

    Reduce

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -82,7 +82,6 @@ console.log(arr.splice(2));
 
 let arr2 = ['f', 'g', 'h', 'i', 'o'];
 const letters = console.log(arr.concat(arr2));
-console.log(letters.join(' - '));
 
 /////  MAP ////////
 
@@ -90,3 +89,26 @@ const eurToUsd = 1.1;
 const movementsUSD = movements.map(mov => mov * eurToUsd);
 const movementsUSDfor = [];
 for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+
+const movementsDecription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'}${Math.abs(
+      mov
+    )}`
+);
+console.log(movementsDecription);
+
+const deposits = movements.filter(mov => mov > 0);
+console.log(deposits);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(balance);
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
